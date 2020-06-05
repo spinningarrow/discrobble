122e9dab95d7d92bbbe2ff9782ae8dfd8fe6ae22
Sahil Bajaj <spinningarrow@users.noreply.github.com>
2020-06-05 23:26:14 +0200

    Play around with some views and UI controls

--- Discrobble/ContentView.swift
+++ Discrobble/ContentView.swift
@@ -8,29 +8,91 @@
 
 import SwiftUI
 
+struct Item: Identifiable {
+    var id = UUID()
+    var value: String
+}
+
 struct ContentView: View {
     @State private var selection = 0
- 
+    @State private var name: String = "Tim"
+    @State private var otherName: String = "Not Tim"
+    @State private var showingAlert = false
+    
+    let collection = [
+        Item(value:"hi"),
+        Item(value:"bye"),
+        Item(value:"wow"),
+    ]
+    
+    let lastFm = [
+        Item(value:"song 2"),
+        Item(value:"song 1"),
+        Item(value:"song 3"),
+    ]
+    
     var body: some View {
         TabView(selection: $selection){
-            Text("First View")
-                .font(.title)
-                .tabItem {
-                    VStack {
-                        Image("first")
-                        Text("First")
-                    }
+            VStack {
+                TextField("Enter your name", text: $name)
+                Text("Hello, \(name)!")
+                
+                TextField("Enter your other name", text: $otherName)
+                Text("Hello other, \(otherName)!")
+                Button(action: {
+                    self.showingAlert = true
+                }) {
+                    Text("Show alert")
                 }
-                .tag(0)
-            Text("Second View")
+            }
+            .tabItem {
+                VStack {
+                    Image("first")
+                    Text("Login")
+                }
+            }
+            .tag(0)
+            
+            List(collection) {item in
+                Text(item.value)
+            }
+                
+            .tabItem {
+                VStack {
+                    Image("second")
+                    Text("Collection")
+                }
+            }
+            .tag(1)
+            
+            List {
+                Button(action: {
+                    self.showingAlert = true
+                }) {
+                    Text("Hello world")
+                }
+                
+                Image(systemName: "clock")
+            }
+            .tabItem {
+                VStack {
+                    Image("second")
+                    Text("Last.fm")
+                }
+            }
+            .tag(2)
+            
+            Text("Settings")
                 .font(.title)
                 .tabItem {
                     VStack {
                         Image("second")
-                        Text("Second")
+                        Text("Settings")
                     }
-                }
-                .tag(1)
+            }
+            .tag(3)
+        }.alert(isPresented: $showingAlert) {
+            Alert(title: Text("Important message"), message: Text("Wear sunscreen"), dismissButton: .default(Text("Got it!")))
         }
     }
 }
